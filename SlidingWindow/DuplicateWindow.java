34143ab24116cc817d3c8e127d3945ba56804392
Greeshma <morsugreeshmareddy@gmail.com>
2026-01-22 23:38:07 +0530

    Updated on 22 Jan 2026

---
+++ SlidingWindow/DuplicateWindow.java
@@ -0,0 +1,76 @@
+// Given an integer array of nums and an integer window size X, 
+// return true if there are duplicate values (nums[i]== nums[j])in that Window(X)
+// X size is always <= abs(i - j) where i and j are two distinct indices of array.
+
+// Input Format:
+// -------------
+// Line-1: An integer N, Array Size
+// Line-2: Space separated integers, array elements
+// Line-3: An integer X, window size
+
+// Output Format:
+// --------------
+// Line-1: Booelan value, true/false
+
+
+// Sample Input-1:
+// ---------------
+// 4
+// 1 2 3 1  
+// 3 
+
+// Sample Output-1: 
+// ----------------
+// false
+
+// Sample Input-2:
+// ---------------
+// 6
+// 1 2 3 3 2 3
+// 2
+
+// Sample Output-2: 
+// ----------------
+// true
+
+
+import java.util.*;
+public class DuplicateWindow
+{
+    public static boolean checkDuplicate(int[] arr,int x)
+    {
+        if(x<=1)
+        {
+            return false;
+        }
+        Set<Integer> s = new HashSet<>();
+        for(int i=0;i<arr.length-1;i++)
+        {
+            if(!s.contains(arr[i]))
+            {
+                s.add(arr[i]);
+            }
+            else
+            {
+                return true;
+            }
+            if(s.size()>x)
+            {
+                s.remove(arr[i-x]);
+            }
+        }
+        return false;
+    }
+    public static void main(String [] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for(int i=0;i<n;i++)
+        {
+            arr[i] = sc.nextInt();
+        }
+        int x = sc.nextInt();
+        System.out.println(checkDuplicate(arr,x));
+    }
+}
