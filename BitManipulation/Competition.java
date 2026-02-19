aa5ac80424462cfcc2d661b54aee6afd080799ee
Greeshma <morsugreeshmareddy@gmail.com>
2026-02-19 16:42:15 +0530

    18 Feb 2026

---
+++ BitManipulation/Competition.java
@@ -0,0 +1,36 @@
+// An English competition is taking place in a school. One problem is given in the 
+// following manner There are words formed with lower case alphabets, participants 
+// need to find the count of characters which are repeatedly appeared in all words. 
+// If no such characters are there, then return 0.
+
+// Note: give solution using bit manipulation only
+
+// Example:
+// ---------
+// input = abc,abc,bc
+// output=2
+// The characters 'b' and 'c' appear in each word, so there output is 2 .
+
+// Input Format: 
+// ---------------
+// comma separated strings.
+
+// Output Format:
+// -------------
+// an integer 		
+
+// SAMPLE INPUT:
+// ------------
+// abcdde,baccd,eeabg
+
+// SAMPLE OUTPUT:
+// -------------
+// 2
+
+// Explanation:
+// -----------
+// Only 'a' and 'b' occur in every word.
+
+// Constraints:
+// --------------
+// Each word consists of only lower-case letters ('a'-'z').
